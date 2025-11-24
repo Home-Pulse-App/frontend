9087ef92362a71c79e2fb07c8522765821428630
Alfredo Chavez <alfredocharom@gmail.com>
2025-11-24 16:47:16 +0100

    style(add tailwindcss): add Tailwindcss
    Add Tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './main.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './main.css';
+import App from './App.tsx';
+// import { HashRouter } from 'react-router';
+//* Use HashRouter instead of BrowserRouter (prevents 404s on refresh/direct links)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <HashRouter> */}
+      <App />
+    {/* </HashRouter> */}
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
