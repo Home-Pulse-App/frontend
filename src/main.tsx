dab671a3d371b4a3e040c23adc80b3e24a08de8f
Alfredo Chavez <alfredocharom@gmail.com>
2025-11-25 12:51:34 +0100

    feat(deviceplacer.tsx): add test to place and move devices
    Add test to place and move devices

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './main.css';
 import App from './App.tsx';
 import { HashRouter } from 'react-router-dom';
+import DevicePlacer from './components/DevicePlacer.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
-      <App />
+      {/* <App /> */}
+      <DevicePlacer />
     </HashRouter>
   </StrictMode>,
 )
