d62672078b0fc97a46decf578b14ed9c85573f47
Alfredo Chavez <alfredocharom@gmail.com>
2025-11-26 18:42:01 +0100

    feat(sensorcontrolpanel): add GlassSurface to SensorControlPanel
    Add GlassSurface to SensorControlPanel

--- src/components/SparkComponent.tsx
+++ src/components/SparkComponent.tsx
@@ -122,7 +122,7 @@ function SparkComponent() {
         {!loading &&
           <>
             {displayInstruction &&
-            <div className='absolute inset-0 z-40 flex items-center justify-center pointer-events-none'>
+            <div className='absolute inset-0 bottom-20 z-40 flex items-center justify-center pointer-events-none'>
               <Instructions></Instructions>
             </div>
             }
