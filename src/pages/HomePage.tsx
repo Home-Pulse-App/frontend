cb19f7acd56a22cd8ce8c509491d106054f0abc5
Alfredo Chavez <alfredocharom@gmail.com>
2025-12-01 13:48:28 +0100

    feat(app, homepage, footer, index, hero): tidy up Home page
    Tidy up Home page, cleaning unused buttons and links and make video responsive and sticky navbar
    controlled

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,31 +3,28 @@ import FooterSimple01 from '@/components/blocks/footer/footer-simple-01';
 import HomePulseLogo from '@/assets/HomePulseLogo.png';
 import Hero from '@/components/blocks/hero/hero';
 import { Process1 } from '@/components/process1';
-import ProgressWithLabel1 from '@/components/examples/progress/with-label/progress-with-label-1';
 import { Separator } from '@/components/ui/separator';
 import '../App.css';
 
 export default function HomePage() {
   return (
     <div className="relative flex min-h-screen flex-col w-full">
-      <Navbar01
-        logo={
-          <img
-            src={HomePulseLogo}
-            alt="HomePulse Logo"
-            className="h-15 w-auto"
-          />
-        }
-        logoHref="/"
-        navigationLinks={[
-          { label: 'Dashboard', href: '/dashboard' },
-          { label: 'Pricing', href: '/pricing' },
-          { label: 'About', href: '/about' },
-        ]}
-      />
-
       <main className="flex-1 px-6 py-6">
-        <div className="mx-auto w-full max-w-screen-xl">
+        <Navbar01
+          logo={
+            <img
+              src={HomePulseLogo}
+              alt="HomePulse Logo"
+              className="h-15 w-auto"
+            />
+          }
+          logoHref="/"
+          navigationLinks={[
+            { label: 'Dashboard', href: '/dashboard' },
+            { label: 'Immersive View', href: '/ImmersiveView' },
+          ]}
+        />
+        <div className="mx-auto w-full max-w-7xl">
           <section className="space-y-2">
             <div className="py-4">
               <Hero />
@@ -35,14 +32,10 @@ export default function HomePage() {
 
             <Separator />
 
-            <div className="py-6">
+            <div id="Process" className="py-6">
               <Process1 />
             </div>
           </section>
-
-          <div className="mx-auto max-w-md mt-2">
-            <ProgressWithLabel1 />
-          </div>
         </div>
       </main>
 
