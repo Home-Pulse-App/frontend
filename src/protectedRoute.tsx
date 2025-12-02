6d02a2cd9a5dcd4c27ae49aaa699cdb05ae7c9ee
matm89 <55142684+matm89@users.noreply.github.com>
2025-12-02 11:57:29 +0100

    fix:lint

--- src/protectedRoute.tsx
+++ src/protectedRoute.tsx
@@ -3,6 +3,6 @@ import { authService } from './services/authService';
 import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRoute() {
-    const isAuthenticated = authService.isAuthenticated();
-    return isAuthenticated ? <Outlet /> : <Navigate to='/login' />;
+  const isAuthenticated = authService.isAuthenticated();
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />;
 }
